764604f8d20a4e7f931afa863a29427b32fdd151
Dorothyhung <dorothyh0602@gmail.com>
2023-02-11 15:16:20 -0800

    add to list page

---
+++ main.js
@@ -0,0 +1,15 @@
+list = [];
+
+var Restaurant = function(name, cuisine, price, city, priority, visited) {
+    this.name=name;
+    this.cuisine=cuisine;
+    this.price=price;
+    this.city=city;
+    this.priority=priority;
+    this.visited=visited;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    //Add to Library
+    
+})
