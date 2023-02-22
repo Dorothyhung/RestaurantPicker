a6a4e11e6ddb52f6c4f70334e56e9eef057c6267
Dorothyhung <dorothyh0602@gmail.com>
2023-02-21 19:27:13 -0800

    added hidden details pages

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ list = [];
 list2 = [];
 idlist = [];
 
-var Restaurant = function(name, cuisine, price, city, priority, visited) {
+var Restaurant = function(name, cuisine, price, city, visited) {
     this.ID=Math.random().toString(16).slice(5);
     this.name=name;
     this.cuisine=cuisine;
     this.price=price;
     this.city=city;
-    this.priority=priority;
     this.visited=visited;
+    this.rID = list.length + 1;
 }
 
 let displayList = document.getElementById("myList");
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             document.getElementById("cuisine").value,
             price,
             document.getElementById("city").value,
-            priority,
             list.length,  // set ID
             document.location.href = "index.html#View"))
         console.log(list)
@@ -92,17 +91,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     this.getElementById("display").addEventListener("click", function() {
-            createList();
+        createList();
     })
 
     this.getElementById("details").addEventListener("click", function() {
+        
         let localID = localStorage.getItem('parm');
-        list = JSON.parse(localStorage.getItem('list'));
+        console.log(localStorage.getItem('parm'))
+        console.log(list[localID - 1]);
+        console.log("In details page")
+        //list = JSON.parse(localStorage.getItem('list'));
         document.getElementById("detailName").innerHTML = "Restaurant: " + list[localID - 1].name;
         document.getElementById("detailCuisine").innerHTML = "Cuisine: " + list[localID - 1].cuisine;
-        document.getElementById("detailPrice").innerHTML = "Price range: " + list[localID - 1].price;
+        document.getElementById("detailPrice").innerHTML = "Price range: " + list[localID - 1].price.value;
         document.getElementById("detailCity").innerHTML = "Location: " + list[localID - 1].city;
-        document.getElementById("detailPriority").innerHTML = "Priority: " + list[localID - 1].priority;
     })
 
 })
@@ -116,19 +118,23 @@ function createList() {
     list.forEach(function(oneRestaurant,) {
         var myLi = document.createElement("li");
         myLi.classList.add("oneRestaurant");
-        myLi.setAttribute("data-parm", oneRestaurant.ID);
-        myLi.innerHTML = oneRestaurant.ID + ": " + oneRestaurant.name;
+        myLi.setAttribute("data-parm", oneRestaurant.rID);
+        myLi.innerHTML = oneRestaurant.rID + ": " + oneRestaurant.name;
         myUL.appendChild(myLi);
     })
 
     var lilist = document.getElementsByClassName("oneRestaurant");
     let newList = Array.from(lilist);
-    newList = forEach(function (element) {
-        var parm = this.getAttribute("data-parm");
-        localStorage.setItem('parm', parm);
-        let stringList = JSON.stringify(list);
-        localStorage.setItem('list', stringList);
 
-        document.location.href="index.html#details";
+    newList.forEach(function (element) {
+        element.addEventListener('click', function() {
+            var parm = this.getAttribute("data-parm");
+            localStorage.setItem('parm', parm);
+            console.log("parm = " + parm);
+            let stringList = JSON.stringify(list);
+            localStorage.setItem('list', stringList);
+
+            document.location.href="index.html#details";
+        })
     })
 }
